e5a145d44d4abecaf0bae5786e8af6118d738b84
Ilya Buravov <ilburale@gmail.com>
2025-11-14 23:42:33 +0300

    doc: connected license and file docstring

--- example/main.c
+++ example/main.c
@@ -12,9 +12,7 @@
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
-*/
 
-/*
   @file example/main.c
   @brief Example program demonstrating library usage
   @author Ilya Buravov (ilburale@gmail.com)
--- include/mylib/apidef.h
+++ include/mylib/apidef.h
@@ -12,9 +12,7 @@
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
-*/
 
-/*
   @file include/mylib/apidef.h
   @brief API declaration macro for C++ compatibility
   @author Ilya Buravov (ilburale@gmail.com)
